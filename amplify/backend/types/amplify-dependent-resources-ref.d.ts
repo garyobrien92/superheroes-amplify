9eb946d70cc8f94a062d86afc9d1cbedc08f7bb4
Gary O Brien <garyobrieneu@gmail.com>
2024-03-17 12:35:40 +1100

    initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,48 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "superheroes": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "auth": {
+    "superheroesapi2cff0e3d": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "superheroesapiaeee2740": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "superheroesfunc": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "superhero": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "SortKeyName": "string",
+      "SortKeyType": "string",
+      "StreamArn": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { Amplify } from 'aws-amplify'
+import config from './aws-exports'
+import '@aws-amplify/ui-react/styles.css';
+import { useEffect, useState } from 'react';
+
+import { get } from 'aws-amplify/api';
+
+Amplify.configure(config)
+
+console.log(Amplify.getConfig())
 
 function App() {
+
+  const [heroes, setHeroes] =  useState([])
+
+  useEffect(() => {
+    get({
+      apiName: 'superheroes', 
+      path:'/superheroes'
+    })
+    .response
+    .then((res) => res.body.json())
+    .then(res => setHeroes(() =>res)).catch(err => {
+      console.log(err)
+    })
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className="App">
+          <header className="App-header">
+            Hello
+            <p>
+            Hey 
+          </p>
+
+          {
+            heroes.map((hero) => (
+              <div key={hero.id}>
+                { hero.name }
+                <img src={hero.image.url} width="50px" height="50px" />
+              </div>
+            ))
+          }
+          </header>
+        </div>
+      
   );
 }
 
